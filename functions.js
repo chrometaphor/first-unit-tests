ce1f56b1ce0ff129479dbb071bfdb93b0c3e0a9e
Chris Cole <christopher.a.cole@gmail.com>
2023-02-02 10:17:56 -0800

    Complete functions

--- functions.js
+++ functions.js
@@ -1,9 +1,47 @@
 // functions.js
-function addTwoNumbers() {
+function addTwoNumbers(num1, num2) {
+     return (num1 + num2)
+
+
 }
+
+function sayHello(name) {
+    if (name === 'Will') {
+       return  'No more testing Will!'
+    } else {
+        return `Hi there ${name}!`
+    }
+}
+
+function buildCar(carColor, carType) {
+    var carDetails = {
+        color: carColor,
+        type: carType
+    }
+    if (carColor || carType) {
+        return carDetails
+    } else {
+        return {}
+    }
+
+
+    // return carDetails
+}
+
+// function buildCar(carColor, carType) {
+//     var carDetails = {
+//     }
+//     carDetails.color = carColor
+//     carDetails.type = carType
+
+//     return carDetails
+// }
 
 module.exports = {
     addTwoNumbers,
-    // sayHello,
-    // buildCar
+    sayHello,
+    buildCar
 }
+
+// color: carColor,
+// type: carType
